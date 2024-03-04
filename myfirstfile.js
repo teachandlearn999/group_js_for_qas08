aeade4d4ff7f77e5a062d46170a679916b740ae8
RimmaSalikh <132116106+RimmaSalikh@users.noreply.github.com>
2024-03-04 13:54:09 -0800

    RimmaCommit (#12)

--- myfirstfile.js
+++ myfirstfile.js
@@ -17,3 +17,16 @@ function sumNums(a, b, c) {
 
 let result = sumNums(a, c, d);
 console.log(result);
+
+function Person(name, age, profession, country){
+    this.name = name
+    this.age = age
+    this.profession = profession
+    this.country = country
+}
+
+const user1 = new Person("John", 25, "developer", "Unites States")
+user1.lastName = 'Doe'
+Person.prototype.nationality = "English"
+console.log(user1)
+console.log(user1.nationality)

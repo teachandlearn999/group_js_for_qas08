da243ed172c586fcc80023f77940f32def7f4a4e
VViktoriia <vvaleter@gmail.com>
2024-03-12 12:54:39 -0700

    Add changed code

---
+++ sampleTestCode.js
@@ -0,0 +1,16 @@
+let number = prompt('type digital');
+let operator = prompt('1.Concatenacia + 2.% remainder 3. умножить  на 5')
+
+if (operator === '1') {
+  console.log(operator + 15);
+}
+else if (operator === '2') {
+    console.log(number % 2);
+}
+else if (operator === '3') {
+    console.log(number * 5);
+}
+else {
+    alert('Operator ${operator} не поддерживается, попробуйте еще раз')
+}
+
